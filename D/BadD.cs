d582efaba639711f9de950a253a49fec708d4674
yishain11 <yishain11@walla.co.il>
2025-06-11 21:17:01 +0300

    added d

---
+++ D/BadD.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_D.D
+{
+    public class Soldier
+    {
+        public string Name { get; set; }
+
+        public void Attack(Terrorist enemy)
+        {
+            Console.WriteLine($"{Name} attacks {enemy.Alias}!");
+            enemy.TakeDamage(10);
+        }
+    }
+
+    public class Terrorist
+    {
+        public string Alias { get; set; }
+        public int Health { get; set; } = 100;
+
+        public void TakeDamage(int amount)
+        {
+            Health -= amount;
+            Console.WriteLine($"{Alias} took {amount} damage. Remaining health: {Health}");
+        }
+    }
+
+    public class BattleArena
+    {
+        public void Fight()
+        {
+            var soldier = new Soldier { Name = "Elite" };
+            var terrorist = new Terrorist { Alias = "Ghost" };
+
+            soldier.Attack(terrorist);
+        }
+    }
+
+    internal class BadD
+    {
+    }
+}
